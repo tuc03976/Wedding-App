d311b2efd7a2cb525eed12e81cb011f9c339c18a
Steven Watson <tuc03976@stevens-macbook-air.local>
2018-02-05 23:52:33 -0500

    over 600

--- Podfile
+++ Podfile
@@ -12,5 +12,6 @@ pod 'Firebase/Core'
 pod 'Firebase/Auth'
 pod 'Firebase/Storage'
 pod 'Firebase/Database'
+pod 'CircleMenu'
 
 end
--- Wedding App/MenuViewController.swift
+++ Wedding App/MenuViewController.swift
@@ -7,8 +7,21 @@
 //
 
 import UIKit
+import CircleMenu
 
-class MenuViewController: UIViewController {
+
+extension UIColor {
+    static func color(_ red: Int, green: Int, blue: Int, alpha: Float) -> UIColor {
+        return UIColor(
+            red: 1.0 / 255.0 * CGFloat(red),
+            green: 1.0 / 255.0 * CGFloat(green),
+            blue: 1.0 / 255.0 * CGFloat(blue),
+            alpha: CGFloat(alpha))
+    }
+}
+
+
+class MenuViewController: UIViewController, CircleMenuDelegate {
     
     
     @IBAction func logoutTapped(_ sender: Any) {
@@ -16,32 +29,65 @@ class MenuViewController: UIViewController {
         
         dismiss(animated: true, completion: nil)
         
-        
-        
-        
-        
-    }
+}
 
+    //    let colors = [UIColor.redColor(), UIColor.grayColor(), UIColor.greenColor(), UIColor.purpleColor()]
+    let items: [(icon: String, color: UIColor)] = [
+        ("icon_home", UIColor(red: 0.19, green: 0.57, blue: 1, alpha: 1)),
+        ("icon_search", UIColor(red: 0.22, green: 0.74, blue: 0, alpha: 1)),
+        ("notifications-btn", UIColor(red: 0.96, green: 0.23, blue: 0.21, alpha: 1)),
+        ("settings-btn", UIColor(red: 0.51, green: 0.15, blue: 1, alpha: 1)),
+        ("nearby-btn", UIColor(red: 1, green: 0.39, blue: 0, alpha: 1)),
+        ]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // add button
+        //        let button = CircleMenu(
+        //            frame: CGRect(x: 200, y: 200, width: 50, height: 50),
+        //            normalIcon:"icon_menu",
+        //            selectedIcon:"icon_close",
+        //            buttonsCount: 4,
+        //            duration: 4,
+        //            distance: 120)
+        //        button.backgroundColor = UIColor.lightGrayColor()
+        //        button.delegate = self
+        //        button.layer.cornerRadius = button.frame.size.width / 2.0
+        //        view.addSubview(button)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: <CircleMenuDelegate>
+    
+    func circleMenu(_: CircleMenu, willDisplay button: UIButton, atIndex: Int) {
+        button.backgroundColor = items[atIndex].color
+        
+        button.setImage(UIImage(named: items[atIndex].icon), for: .normal)
+        
+        // set highlited image
+        let highlightedImage = UIImage(named: items[atIndex].icon)?.withRenderingMode(.alwaysTemplate)
+        button.setImage(highlightedImage, for: .highlighted)
+        button.tintColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
     }
-    */
-
+    
+    func circleMenu(_: CircleMenu, buttonWillSelected _: UIButton, atIndex: Int) {
+        print("button will selected: \(atIndex)")
+        switch atIndex {
+        case 0:
+            print("The Cow Wins!")
+            performSegue(withIdentifier: "Go", sender: self)
+        case 1:
+            performSegue(withIdentifier: "Go", sender: self)
+            print("The Pig Wins!")
+        default:
+            print("Keep Trying")
+        }
+        
+        
+    }
+    
+    
+    func circleMenu(_: CircleMenu, buttonDidSelected _: UIButton, atIndex: Int) {
+        print("button did selected: \(atIndex)")
+    }
 }
+
